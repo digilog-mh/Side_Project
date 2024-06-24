38f972155b7ff339fb25a60077d7c08d7b02741c
Minha <yu6jin@gmail.com>
2024-06-24 21:57:29 +0900

    #14 - 도메일 코드를 엔티티로 변환
    - 베이스 엔티티 삭제
    -도메인 클래스 == 1테이블 매칭되도록 설계.
    -생성자의 equals(), hashcode() 등이 도메인 맥락에 맞게 수정.

--- board-project/src/main/java/com/side/boardproject/domain/ArticleComment.java
+++ board-project/src/main/java/com/side/boardproject/domain/ArticleComment.java
@@ -1,8 +1,65 @@
 package com.side.boardproject.domain;
 
-public class ArticleComment extends BaseEntity{
-    private Long id; //게시글 id
-    private String content; // 내용
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-    private Article article; // 게시글
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes ={
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class ArticleComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //게시글 id
+
+    @Setter @Column(nullable = false, length = 250) private String content; // 내용
+    @Setter @ManyToOne(optional = false) private Article article; // 게시글
+
+    @CreatedDate
+    @Column(nullable = false) private LocalDateTime createdAt; //생성일시
+    @CreatedBy
+    @Column(nullable = false) private String createdBy; //생성자
+    @LastModifiedDate
+    @Column(nullable = false) private LocalDateTime modifiedAt; //수정일시
+    @LastModifiedBy
+    @Column(nullable = false) private String modifiedBy; //수정자
+
+    public ArticleComment() {
+    }
+
+    private ArticleComment(String content, Article article) {
+        this.content = content;
+        this.article = article;
+    }
+
+    public static ArticleComment of(String content, Article article) {
+        return new ArticleComment(content, article);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment that)) return false;
+        return id != null && (Objects.equals(id, that.id));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
