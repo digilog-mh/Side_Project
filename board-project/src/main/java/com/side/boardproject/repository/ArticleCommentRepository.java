a3718efa90725b624f508bac152c4463c372a2c7
Minha <yu6jin@gmail.com>
2024-06-24 21:58:06 +0900

    # 14 - Repository interface 작성
    DB 접근.

---
+++ board-project/src/main/java/com/side/boardproject/repository/ArticleCommentRepository.java
@@ -0,0 +1,7 @@
+package com.side.boardproject.repository;
+
+import com.side.boardproject.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
